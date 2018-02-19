7cfd334de2cd9ee0e9310c18ed7d86b2ed9c5b8b
MaxChillin <jeremy.pierce0011@gmail.com>
2018-02-18 20:57:46 -0600

    Fun With C++
    These are just some file I made while trying to get better at C++

---
+++ FunWithCPlusPlus/FunWithInheritance.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//#include <string>
+//
+//class Entity
+//{
+//public:
+//	float X, Y;
+//
+//	void move(float xa, float ya) {
+//		X += xa;
+//		Y += ya;
+//	}
+//
+//
+//private:
+//
+//};
+//
+//class Player : public Entity {
+//public:
+//	std::string Name;
+//
+//	void printName() {
+//		std::cout << Name << std::endl;
+//	}
+//
+//	void setName(std::string name) {
+//		Name = name;
+//	}
+//};
+//
+//int main() {
+//	Player player;
+//	player.move(5.0, 5.0);
+//	player.Name = "bob";
+//	player.printName();
+//	std::cin.get();
+//}
---
+++ FunWithCPlusPlus/FunWithMemberInitializerLists.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//
+//class Entity {
+//public:
+//	int x, y;
+//	
+//	Entity() :
+//		x(0), y(2)
+//	{
+//		std::cout << "Entity created with x = " << x << " and y = " << y << std::endl;
+//	}
+//
+//	
+//
+//};
+//int main() {
+//	Entity e;
+//	std::cin.get();
+//}
