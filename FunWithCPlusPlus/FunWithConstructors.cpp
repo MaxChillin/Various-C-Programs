5def2bad70c1eb331b133cf636c9f619db025a39
MaxChillin <jeremy.pierce0011@gmail.com>
2018-02-18 20:58:59 -0600

    Update FunWithConstructors.cpp

--- FunWithCPlusPlus/FunWithConstructors.cpp
+++ FunWithCPlusPlus/FunWithConstructors.cpp
@@ -1,31 +1,31 @@
-//#include <iostream>
-//
-//class Entity
-//{
-//public:
-//	int x, y;
-//
-//	Entity() {
-//		x = 0;
-//		y = 0;
-//		std::cout << "Created" << std::endl;
-//	}
-//
-//	~Entity() {
-//		std::cout << "Destroyed" << std::endl;
-//	}
-//	void print() {
-//		std::cout << x << " " << y << std::endl;
-//	}
-//
-//};
-//
-//int main() {
-//	{// start of scope
-//	Entity e;
-//	e.print();
-//
-//	}// end of scope
-//	std::cin.get();
-//}
-//
+#include <iostream>
+
+class Entity
+{
+public:
+	int x, y;
+
+	Entity() {
+		x = 0;
+		y = 0;
+		std::cout << "Created" << std::endl;
+	}
+
+	~Entity() {
+		std::cout << "Destroyed" << std::endl;
+	}
+	void print() {
+		std::cout << x << " " << y << std::endl;
+	}
+
+};
+
+int main() {
+	{// start of scope
+	Entity e;
+	e.print();
+
+	}// end of scope
+	std::cin.get();
+}
+
